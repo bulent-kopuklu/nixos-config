dd308afdec28b645722db6b54e24d6e0466a5413
bulentk <bulent.kopuklu@gmx.com.tr>
2021-01-13 02:38:54 +0300

    .

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,7 @@
 
 {
     imports = [
-        ./hardware-configuration.nix
+        ../hardware-configuration.nix
         ./machines/configurations.nix
         ./windowing/configurations.nix
     ];
