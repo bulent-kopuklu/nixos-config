bf2be9581f8caddd878791c93a256a5ba03a6a1a
bulentk <bulent.kopuklu@gmx.com.tr>
2021-01-17 22:59:07 +0300

    .

--- env/i3wm-none.nix
+++ env/i3wm-none.nix
@@ -1,33 +1,5 @@
 { config, pkgs, expr, buildVM, ... }:
 
-let
-    iconTheme = pkgs.breeze-icons.out;
-    themeEnv = ''
-        # QT: remove local user overrides (for determinism, causes hard to find bugs)
-        rm -f ~/.config/Trolltech.conf
-
-        # GTK3: remove local user overrides (for determinisim, causes hard to find bugs)
-        rm -f ~/.config/gtk-3.0/settings.ini
-
-        # GTK3: add breeze theme to search path for themes
-        # (currently, we need to use gnome-breeze because the GTK3 version of breeze is broken)
-        export XDG_DATA_DIRS="${pkgs.gnome-breeze}/share:$XDG_DATA_DIRS"
-
-        # GTK3: add /etc/xdg/gtk-3.0 to search path for settings.ini
-        # We use /etc/xdg/gtk-3.0/settings.ini to set the icon and theme name for GTK 3
-        export XDG_CONFIG_DIRS="/etc/xdg:$XDG_CONFIG_DIRS"
-
-        # GTK2 theme + icon theme
-        export GTK2_RC_FILES=${pkgs.writeText "iconrc" ''gtk-icon-theme-name="breeze"''}:${pkgs.breeze-gtk}/share/themes/Breeze/gtk-2.0/gtkrc:$GTK2_RC_FILES
-
-        # SVG loader for pixbuf (needed for GTK svg icon themes)
-        export GDK_PIXBUF_MODULE_FILE=$(echo ${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/*/loaders.cache)
-
-        # LS colors
-        eval `${pkgs.coreutils}/bin/dircolors "${./dircolors}"`
-    '';
-
-in 
 {
 
     imports = [];
@@ -136,24 +108,16 @@ in
         # here anyway
     '';
 
-    # QT4/5 global theme
-    environment.etc."xdg/Trolltech.conf" = {
-        text = ''
-            [Qt]
-            style=Breeze
-        '';
-        mode = "444";
-    };
 
     # GTK3 global theme (widget and icon theme)
-    environment.etc."xdg/gtk-3.0/settings.ini" = {
-        text = ''
-            [Settings]
-            gtk-icon-theme-name=breeze
-            gtk-theme-name=Breeze-gtk
-        '';
-        mode = "444";
-    };
+#    environment.etc."xdg/gtk-3.0/settings.ini" = {
+#        text = ''
+#            [Settings]
+#            gtk-icon-theme-name=breeze
+#            gtk-theme-name=Breeze-gtk
+#        '';
+#        mode = "444";
+#    };
 
     environment.systemPackages = with pkgs; [
         # i3 desktop support
@@ -191,13 +155,7 @@ in
         xlibs.libXScrnSaver # for argyllcms
 
         # GTK theme
-        breeze-gtk
-        gnome-breeze
         gnome3.gnome_themes_standard
-
-        # Qt theme
-        breeze-qt5
-        # breeze-qt4
 
         # Icons (Main)
         iconTheme
