957886f265bc64893739791df6a582b971d3e1fd
bulentk <bulent.kopuklu@gmx.com.tr>
2021-05-25 16:52:31 +0300

    version update

--- pkgs/overlays/smartgithg.nix
+++ pkgs/overlays/smartgithg.nix
@@ -1,10 +1,10 @@
 self: super: {
   smartgithg = super.smartgithg.overrideAttrs(oldAttrs: rec {
     pname = "smartgithg";
-    version = "20.2.4";
+    version = "20.2.5";
     src = super.fetchurl {
       url = "https://www.syntevo.com/downloads/smartgit/smartgit-linux-${builtins.replaceStrings [ "." ] [ "_" ] version}.tar.gz";
-      sha256 = "1iamj1by5clb7avddm749fhw1gyg2m0i1lkyk867vzap966wp7b4";
+      sha256 = "05f3yhzf6mvr6c5v6qvjrx97pzrrnkh9mp444zlkbnpgnrsmdc6v";
     };
   });
 }
