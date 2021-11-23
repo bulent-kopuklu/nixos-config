055509ecf3920fb8cb3e09a61c521de95809f4ce
bulent-kopuklu <bulent.kopuklu@gmx.com.tr>
2021-11-24 00:12:16 +0300

    asus

---
+++ sys/asus-g14/default.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+      ./hardware-configuration.nix
+      ../../env/development.nix
+    ];
+
+  services = {
+    printing = {
+      drivers = [ pkgs.hplip ]; # todo add ofice printer samsung
+    };
+  };
+
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking.hostName = "bulentk-g14";
+
+  hardware = {
+    opengl.driSupport32Bit = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    zeroad
+    zeroadPackages.zeroad-data
+  ];
+}
