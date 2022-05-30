742c94eae13be20ff33a55211f8af460f21c8b5c
bulent-kopuklu <bulent.kopuklu@gmx.com.tr>
2022-05-30 11:45:06 +0300

    changed screen layout of encom

--- hosts/bulentk-g14/autorandr/encom/config.nix
+++ hosts/bulentk-g14/autorandr/encom/config.nix
@@ -8,21 +8,21 @@
     off
     output HDMI-A-1-0
     off
-    output eDP-1-0
-    crtc 4
-    mode 1920x1080
-    pos 0x0
-    rate 60.01
-    output DP-0.8
-    crtc 0
-    mode 2560x1440
-    pos 1920x0
-    primary
-    rate 59.95
     output DP-0.1
     crtc 1
     mode 2560x1440
-    pos 4480x0
+    pos 0x0
     rate 59.95
+    output DP-0.8
+    crtc 0
+    mode 2560x1440
+    pos 2560x0
+    primary
+    rate 59.95
+    output eDP-1-0
+    crtc 4
+    mode 1920x1080
+    pos 5131x0
+    rate 60.01
   '';
 }
