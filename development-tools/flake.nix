b8fd8801fb22cd2ee1910bda632f34b3f703c110
bulentk <bulent.kopuklu@gmx.com.tr>
2026-04-13 00:59:58 +0300

    refactor: enhance flake.nix for improved cross-compilation support and toolchain management

--- development-tools/flake.nix
+++ development-tools/flake.nix
@@ -7,104 +7,129 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system;};
+      let 
+        pkgs = import nixpkgs { inherit system;};
       in {
-        lib = {
-          cpp = { stdenv ? "clang", targetArch ? null, extraPackages ? [] }:
+        lib = rec {
+          cpp = { targets ? [], clang-version ? null, extra-packages ? [] }:
             let
-              cross-pkgs =
-                if targetArch == null           then pkgs
-                else if targetArch == "aarch64" then pkgs.pkgsCross.aarch64-multiplatform
-                else if targetArch == "x86_64"  then pkgs.pkgsCross.gnu64
-                else throw "Unsupported target architecture: ${targetArch}";
+              clang-pkgs = if clang-version == null then pkgs.clang
+                      else pkgs.clang.override { version = clang-version; };
 
-              cc =
-                if stdenv == "clang" then cross-pkgs.clang
-                else if stdenv == "gcc" then cross-pkgs.gcc
-                else throw "Unsupported stdenv: ${stdenv}";
+              cross-toolchains = map (target:
+                if target == "aarch64"    then pkgs.pkgsCross.aarch64-multiplatform.clang
+                else if target == "armv7" then pkgs.pkgsCross.armv7l-hf-multiplatform.clang
+                else throw "Unsupported target: ${target}"
+              ) targets;
 
             in {
               packages = with pkgs; [
+                clang-tools
+                gdb
+                lldb
                 cmake
                 ninja
-                gdb
-                cc
-              ] ++ extraPackages;
+                gnumake
+                pkg-config
+                ccache
+                glibc.static
+                musl
+              ] ++ [ clang-pkgs ] ++ cross-toolchains ++ extra-packages;
 
               shellHook = ''
-                export CC="${cc}/bin/${if stdenv == "clang" then "clang" else "gcc"}"
-                export CXX="${cc}/bin/${if stdenv == "clang" then "clang++" else "g++"}"
-                ${if targetArch != null then ''
-                  echo "Cross-compilation target: ${targetArch}"
-                '' else ""}
+                export CC="${clang-pkgs}/bin/clang"
+                export CXX="${clang-pkgs}/bin/clang++"
               '';
-            };    
+            };          
           
-          rust = { targetArch ? null, extraPackages ? [] }:
+          rust = { targets ? [], extra-packages ? [] }:
             let
-              rust-target =
-                if targetArch == null           then null
-                else if targetArch == "aarch64" then "aarch64-unknown-linux-gnu"
-                else if targetArch == "x86_64"  then "x86_64-unknown-linux-gnu"
-                else throw "Unsupported target architecture: ${targetArch}";
+              cross-linkers = map (target:
+                if target == "aarch64"    then pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc
+                else if target == "armv7" then pkgs.pkgsCross.armv7l-hf-multiplatform.stdenv.cc
+                else throw "Unsupported target: ${target}"
+              ) targets;
 
-              cross-pkgs =
-                if targetArch == null           then null
-                else if targetArch == "aarch64" then pkgs.pkgsCross.aarch64-multiplatform
-                else if targetArch == "x86_64"  then pkgs.pkgsCross.gnu64
-                else throw "Unsupported target architecture: ${targetArch}";
+              rust-targets = map (target:
+                if target == "aarch64"    then "aarch64-unknown-linux-gnu"
+                else if target == "armv7" then "armv7-unknown-linux-gnueabihf"
+                else throw "Unsupported target: ${target}"
+              ) targets;
 
-              linker =
-                if cross-pkgs == null then null
-                else "${cross-pkgs.stdenv.cc}/bin/${cross-pkgs.stdenv.cc.targetPrefix}cc";
+              linker-configs = map (target:
+                if target == "aarch64" then ''
+                  [target.aarch64-unknown-linux-gnu]
+                  linker = "${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc.targetPrefix}cc"
+                ''
+                else if target == "armv7" then ''
+                  [target.armv7-unknown-linux-gnueabihf]
+                  linker = "${pkgs.pkgsCross.armv7l-hf-multiplatform.stdenv.cc}/bin/${pkgs.pkgsCross.armv7l-hf-multiplatform.stdenv.cc.targetPrefix}cc"
+                ''
+                else throw "Unsupported target: ${target}"
+              ) targets;
+
             in {
-              packages = with pkgs; [
-                rustup
-              ] 
-              ++ (if cross-pkgs != null then [ cross-pkgs.stdenv.cc ] else []) 
-              ++ extraPackages;
+              packages = [ pkgs.rustup ] ++ cross-linkers ++ extra-packages;
+
               shellHook = ''
-                ${if rust-target != null then ''
+                rustup component add rust-analyzer clippy rustfmt 2>/dev/null || true
+                ${if targets != [] then ''
+                  rustup target add ${builtins.concatStringsSep " " rust-targets} 2>/dev/null || true
                   mkdir -p .cargo
                   cat > .cargo/config.toml << EOF
-[target.${rust-target}]
-linker = "${linker}"
-EOF
-                  export CARGO_BUILD_TARGET="${rust-target}"
+          ${builtins.concatStringsSep "\n" linker-configs}
+          EOF
                 '' else ""}
               '';
             };
 
           android = { apiLevel ? 21, ndkVersion ? "23.2.8568313", cmakeVersion ? "3.22.1", extraPackages ? [] }:
             let 
-              androidSdk = pkgs.androidenv.androidPkgs {
+              pkgs-android = import nixpkgs {
+                inherit system;
+                config = {
+                  android_sdk.accept_license = true;
+                  allowUnfree = true;
+                };
+              };
+              androidComposition = pkgs-android.androidenv.composeAndroidPackages {
                 platformVersions = [ (toString apiLevel) ];
                 ndkVersions = [ ndkVersion ];
                 cmakeVersions = [ cmakeVersion ];
                 includeNDK = true;
+                includeEmulator = false;
               };
+              android-sdk = androidComposition.androidsdk;
               android-scripts = pkgs.stdenv.mkDerivation {
                 name = "android-scripts";
                 src = ./android-scripts;
                 installPhase = ''
-                  mkdir -p $out/bin
-                  cp cmake $out/bin/cmake-android
+                  mkdir -p $out/bin $out/share
+                  cp cmake-android $out/bin/cmake-android
                   cp definition $out/bin/definition
+                  cp tasks.json $out/share/tasks-cmake.json
                   chmod +x $out/bin/cmake-android
                 '';
               };
             in {
               packages = [
-                androidSdk.androidsdk
+                android-sdk 
+                pkgs.jdk17
                 android-scripts
               ] ++ extraPackages;
               shellHook = ''
-                export ANDROID_SDK_HOME="${androidSdk.androidsdk}/libexec/android-sdk"
+                export ANDROID_SDK_HOME="${android-sdk}/libexec/android-sdk"
                 export ANDROID_NDK_VERSION="${ndkVersion}"
                 export ANDROID_CMAKE_VERSION="${cmakeVersion}"
                 export ANDROID_MIN_SDK="${toString apiLevel}"
+                export JAVA_HOME="${pkgs.jdk17.home}"
+                if [ ! -f .vscode/tasks-cmake.json ]; then
+                  mkdir -p .vscode
+                  cp ${android-scripts}/share/tasks-cmake.json .vscode/tasks-cmake.json
+                  chmod 666 .vscode/tasks-cmake.json
+                fi
               '';
             };
 
@@ -146,15 +171,15 @@ EOF
             '';
           };
 
-          merge  = toolList: {
-            packages = builtins.concatLists (map (t: t.packages) toolList);
-            shellHook = builtins.concatStringsSep "\n" (map (t: t.shellHook) toolList);
-          };
-
           run-zsh = ''
             export SHELL=${pkgs.zsh}/bin/zsh
             exec ${pkgs.zsh}/bin/zsh --login
           '';
+
+          merge  = toolList: {
+            packages = builtins.concatLists (map (t: t.packages) toolList);
+            shellHook = builtins.concatStringsSep "\n" (map (t: t.shellHook) toolList) + run-zsh;
+          };
         };
       }
     );
